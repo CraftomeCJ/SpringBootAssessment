f278c151fc6206a9a7b5fd3b29eb027ff2646af5
craftomeCJ <tech@craftome.com>
2022-03-14 10:21:54 +0800

    First commit

---
+++ src/main/java/org/generation/SpringBootAssessment/SpringBootAssessmentApplication.java
@@ -0,0 +1,13 @@
+package org.generation.SpringBootAssessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAssessmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/generation/SpringBootAssessment/SpringBootAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package org.generation.SpringBootAssessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
